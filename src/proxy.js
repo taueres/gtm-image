df54bb51e2c794ebe746b51a4e996091a3cae803
Sergio Santoro <santoro.srg@gmail.com>
2018-10-03 12:32:46 +0200

    Add dynamic parameters for URL

--- src/proxy.js
+++ src/proxy.js
@@ -2,8 +2,15 @@ const fetch = require('node-fetch');
 const config = require('../config.json');
 
 exports.fetchImage = async () => {
+    const now = Date.now();
+    const initialTime = 1533679200000; // 08/08/2018
+
+    const url = config.url
+        .replace('{{FROM}}', initialTime)
+        .replace('{{TO}}', now);
+
     const resp = await fetch(
-        config.url,
+        url,
         {
             headers: {
                 'Authorization': 'Bearer ' + config.token
